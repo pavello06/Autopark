9fa0e8dfd064773739f10f5cfe23b4021e988476
Pavel Halukha <pavelgaluha@gmail.com>
2025-02-27 12:54:32 +0300

    add methods to class Car

--- Car/Car.cs
+++ Car/Car.cs
@@ -9,5 +9,29 @@
         public string Model { get; }
         public uint Price { get; protected set; }
         public Engine Engine { get; protected set; }
+
+        static Car()
+        {
+            count++;
+        }
+        public Car(string brand, string model, uint price, EngineType type)
+        {
+            id = count;
+            Brand = brand;
+            Model = model;
+            Price = price;
+            Engine = new Engine(type);
+        }
+
+        protected virtual void UpdatePrice(EngineType oldEngineType, EngineType newEngineType)
+        {
+            Price *= (uint)(Engine.PricesCoefficients[(int)newEngineType] / Engine.PricesCoefficients[(int)oldEngineType]);
+        }
+
+        public void ChangeEngine(Engine newEngine)
+        {
+            UpdatePrice(Engine.Type, newEngine.Type);
+            Engine = newEngine;
+        }
     }
 }
