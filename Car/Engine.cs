1b5a3000a87a06149b4eb27f12647a7ad0cf1aa2
Pavel Halukha <pavelgaluha@gmail.com>
2025-02-27 12:46:20 +0300

    create enum EngineType

--- Car/Engine.cs
+++ Car/Engine.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Autopark.Car
+﻿namespace Autopark.Car
 {
+    internal enum EngineType
+    {
+        Petrol,
+        Gas,
+        Electric
+    }
+
     internal class Engine
     {
     }
