75413bcacbf5c7247a4b73114eb95bc6400147f9
Pavel Halukha <pavelgaluha@gmail.com>
2025-02-27 13:04:18 +0300

    add class RaceCar

---
+++ Car/Race/RaceCar.cs
@@ -0,0 +1,14 @@
+﻿namespace Autopark.Car.Race
+{
+    internal class RaceCar : Car
+    {
+        public uint Acceleration { get; }
+        public uint MaxSpeed { get; }
+
+        public RaceCar(string brand, string model, uint price, EngineType type, uint acceleration, uint maxSpeed) : base(brand, model, price, type)
+        {
+            Acceleration = acceleration;
+            MaxSpeed = maxSpeed;
+        }
+    }
+}
