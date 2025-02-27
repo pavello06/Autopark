c5b1e9cf66ea2faef16d002c8264c1e2cb459b4d
Pavel Halukha <pavelgaluha@gmail.com>
2025-02-27 13:06:40 +0300

    add class RareCar

---
+++ Car/Rare/RareCar.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace Autopark.Car.Rare
+{
+    internal class RareCar : Car
+    {
+        private uint carEvaluationYear;
+        public uint Year { get; }
+        public string FirstOwner { get; }
+
+        public RareCar(string brand, string model, uint price, EngineType type, uint carEvaluationYear, uint year, string firstOwner) : base(brand, model, price, type)
+        {
+            this.carEvaluationYear = carEvaluationYear;
+            Year = year;
+            FirstOwner = firstOwner;
+        }
+
+        protected override void UpdatePrice(EngineType oldEngineType, EngineType newEngineType)
+        {
+            Price = 0;
+        }
+
+        public void UpdatePrice()
+        {
+            Price += (uint)((carEvaluationYear - DateTime.Now.Year) * 100);
+        }
+    }
+}
