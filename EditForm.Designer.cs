9fd154226ac0c5411121ded7e629a96c321bb633
Pavel Halukha <pavelgaluha@gmail.com>
2025-03-14 22:53:47 +0300

    feat: add edit logic

---
+++ EditForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Autopark
+{
+    partial class EditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EditForm));
+            logoPanel = new Panel();
+            titleLabel = new Label();
+            iconPictureBox = new PictureBox();
+            fieldsFlowLayoutPanel = new FlowLayoutPanel();
+            editButton = new Button();
+            logoPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)iconPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // logoPanel
+            // 
+            logoPanel.Controls.Add(titleLabel);
+            logoPanel.Controls.Add(iconPictureBox);
+            logoPanel.Dock = DockStyle.Top;
+            logoPanel.Location = new Point(0, 0);
+            logoPanel.Name = "logoPanel";
+            logoPanel.Size = new Size(282, 46);
+            logoPanel.TabIndex = 5;
+            // 
+            // titleLabel
+            // 
+            titleLabel.AutoSize = true;
+            titleLabel.Dock = DockStyle.Right;
+            titleLabel.Font = new Font("Bookman Old Style", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            titleLabel.Location = new Point(151, 0);
+            titleLabel.Name = "titleLabel";
+            titleLabel.Size = new Size(131, 44);
+            titleLabel.TabIndex = 3;
+            titleLabel.Text = "YPark";
+            // 
+            // iconPictureBox
+            // 
+            iconPictureBox.Dock = DockStyle.Left;
+            iconPictureBox.Image = (Image)resources.GetObject("iconPictureBox.Image");
+            iconPictureBox.Location = new Point(0, 0);
+            iconPictureBox.Name = "iconPictureBox";
+            iconPictureBox.Size = new Size(89, 46);
+            iconPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            iconPictureBox.TabIndex = 2;
+            iconPictureBox.TabStop = false;
+            // 
+            // fieldsFlowLayoutPanel
+            // 
+            fieldsFlowLayoutPanel.AutoScroll = true;
+            fieldsFlowLayoutPanel.Location = new Point(16, 97);
+            fieldsFlowLayoutPanel.Name = "fieldsFlowLayoutPanel";
+            fieldsFlowLayoutPanel.Size = new Size(250, 201);
+            fieldsFlowLayoutPanel.TabIndex = 6;
+            // 
+            // editButton
+            // 
+            editButton.Enabled = false;
+            editButton.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            editButton.Location = new Point(16, 345);
+            editButton.Name = "editButton";
+            editButton.Size = new Size(250, 75);
+            editButton.TabIndex = 7;
+            editButton.Text = "Edit";
+            editButton.UseVisualStyleBackColor = true;
+            editButton.Click += editButton_Click;
+            // 
+            // EditForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(282, 432);
+            Controls.Add(editButton);
+            Controls.Add(fieldsFlowLayoutPanel);
+            Controls.Add(logoPanel);
+            Name = "EditForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Edit";
+            logoPanel.ResumeLayout(false);
+            logoPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)iconPictureBox).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel logoPanel;
+        private Label titleLabel;
+        private PictureBox iconPictureBox;
+        private FlowLayoutPanel fieldsFlowLayoutPanel;
+        private Button editButton;
+    }
+}
---
+++ EditForm.cs
@@ -0,0 +1,103 @@
+﻿using Autopark.Car;
+using System.Reflection;
+
+namespace Autopark
+{
+    public partial class EditForm : Form
+    {
+        private Car.Car _car;
+
+        internal EditForm(Car.Car car)
+        {
+            InitializeComponent();
+
+            _car = car;
+            AddCarFields(car.GetType());
+        }
+
+        private void AddCarFields(Type? type)
+        {
+            if (type!.BaseType != typeof(Object))
+            {
+                AddCarFields(type.BaseType);
+            }
+
+            foreach (var field in type.GetProperties())
+            {
+                if (type.BaseType != null && type.BaseType.GetProperty(field.Name, BindingFlags.Public | BindingFlags.Instance) != null)
+                {
+                    continue;
+                }
+
+                var textBox = new TextBox();
+                textBox.Font = new Font("Segoe UI", 13.8F);
+                textBox.PlaceholderText = field.Name;
+                textBox.Size = new Size(fieldsFlowLayoutPanel.Width - 30, 40);
+                textBox.TextChanged += textBox_TextChanged;
+
+                fieldsFlowLayoutPanel.Controls.Add(textBox);
+            }
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            editButton.Enabled = CanEdit();
+        }
+
+        private bool CanEdit()
+        {
+            foreach (var control in fieldsFlowLayoutPanel.Controls)
+            {
+                if (((TextBox)control).Text.Length == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+
+            Type currentType = _car.GetType();
+            var properties = new List<PropertyInfo>();
+
+            while (currentType != null)
+            {
+                var currentProperties = currentType.GetProperties(
+                    BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly
+                );
+
+                properties.InsertRange(0, currentProperties);
+
+                currentType = currentType.BaseType;
+            }
+
+            foreach (var property in properties)
+            {
+                if (i < fieldsFlowLayoutPanel.Controls.Count && fieldsFlowLayoutPanel.Controls[i] is TextBox textBox)
+                {
+                    object convertedValue;
+                    if (property.PropertyType == typeof(Engine))
+                    {
+                        convertedValue = textBox.Text;
+                        _car.ChangeEngine(new Car.Engine((EngineType)Enum.Parse(typeof(EngineType), ((string)convertedValue))));
+                    }
+                    else
+                    {
+                        convertedValue = Convert.ChangeType(textBox.Text, property.PropertyType);
+                        property.SetValue(_car, convertedValue);
+                    }
+
+                    i++;
+                }
+            }
+
+            Program.Cars.UpdateView();
+            Program.Cars.UpdateHistory();
+
+            this.Close();
+        }
+    }
+}
