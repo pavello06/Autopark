0f13864c7595065bd46efdb9c4ff1008186534c3
Pavel Halukha <pavelgaluha@gmail.com>
2025-03-14 22:53:21 +0300

    feat: add 'add logic'

---
+++ AddForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Autopark
+{
+    partial class AddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddForm));
+            addButton = new Button();
+            carTypeComboBox = new ComboBox();
+            fieldsFlowLayoutPanel = new FlowLayoutPanel();
+            logoPanel = new Panel();
+            titleLabel = new Label();
+            iconPictureBox = new PictureBox();
+            logoPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)iconPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // addButton
+            // 
+            addButton.Enabled = false;
+            addButton.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            addButton.Location = new Point(16, 345);
+            addButton.Name = "addButton";
+            addButton.Size = new Size(250, 75);
+            addButton.TabIndex = 1;
+            addButton.Text = "Add";
+            addButton.UseVisualStyleBackColor = true;
+            addButton.Click += addButton_Click;
+            // 
+            // carTypeComboBox
+            // 
+            carTypeComboBox.AutoCompleteCustomSource.AddRange(new string[] { "Rare", "Race", "Truck", "Passenger", "Bus" });
+            carTypeComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            carTypeComboBox.FormattingEnabled = true;
+            carTypeComboBox.ItemHeight = 20;
+            carTypeComboBox.Location = new Point(16, 78);
+            carTypeComboBox.Name = "carTypeComboBox";
+            carTypeComboBox.Size = new Size(250, 28);
+            carTypeComboBox.TabIndex = 2;
+            carTypeComboBox.SelectedIndexChanged += carTypeComboBox_SelectedIndexChanged;
+            // 
+            // fieldsFlowLayoutPanel
+            // 
+            fieldsFlowLayoutPanel.AutoScroll = true;
+            fieldsFlowLayoutPanel.Location = new Point(15, 124);
+            fieldsFlowLayoutPanel.Name = "fieldsFlowLayoutPanel";
+            fieldsFlowLayoutPanel.Size = new Size(250, 201);
+            fieldsFlowLayoutPanel.TabIndex = 3;
+            // 
+            // logoPanel
+            // 
+            logoPanel.Controls.Add(titleLabel);
+            logoPanel.Controls.Add(iconPictureBox);
+            logoPanel.Dock = DockStyle.Top;
+            logoPanel.Location = new Point(0, 0);
+            logoPanel.Name = "logoPanel";
+            logoPanel.Size = new Size(282, 46);
+            logoPanel.TabIndex = 4;
+            // 
+            // titleLabel
+            // 
+            titleLabel.AutoSize = true;
+            titleLabel.Dock = DockStyle.Right;
+            titleLabel.Font = new Font("Bookman Old Style", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            titleLabel.Location = new Point(151, 0);
+            titleLabel.Name = "titleLabel";
+            titleLabel.Size = new Size(131, 44);
+            titleLabel.TabIndex = 3;
+            titleLabel.Text = "YPark";
+            // 
+            // iconPictureBox
+            // 
+            iconPictureBox.Dock = DockStyle.Left;
+            iconPictureBox.Image = (Image)resources.GetObject("iconPictureBox.Image");
+            iconPictureBox.Location = new Point(0, 0);
+            iconPictureBox.Name = "iconPictureBox";
+            iconPictureBox.Size = new Size(89, 46);
+            iconPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            iconPictureBox.TabIndex = 2;
+            iconPictureBox.TabStop = false;
+            // 
+            // AddForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(282, 432);
+            Controls.Add(logoPanel);
+            Controls.Add(fieldsFlowLayoutPanel);
+            Controls.Add(carTypeComboBox);
+            Controls.Add(addButton);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "AddForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Add";
+            logoPanel.ResumeLayout(false);
+            logoPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)iconPictureBox).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button addButton;
+        private ComboBox carTypeComboBox;
+        private FlowLayoutPanel fieldsFlowLayoutPanel;
+        private Panel logoPanel;
+        private Label titleLabel;
+        private PictureBox iconPictureBox;
+    }
+}
---
+++ AddForm.cs
@@ -0,0 +1,101 @@
+﻿using Autopark.Car;
+using System.Reflection;
+
+namespace Autopark
+{
+    public partial class AddForm : Form
+    {
+        public AddForm()
+        {
+            InitializeComponent();
+
+            AddCarTypes();
+        }
+
+        private void AddCarTypes()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var allTypes = assembly.GetTypes();
+
+            var baseClassType = typeof(CarCreator);
+
+            foreach (var type in allTypes)
+            {
+                if (type != baseClassType && baseClassType.IsAssignableFrom(type))
+                {
+                    carTypeComboBox.Items.Add(type.Name);
+                }
+            }
+        }
+
+        private void carTypeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var carCreatorType = assembly.GetType($"Autopark.Car.{carTypeComboBox.SelectedItem}");
+
+            var carCreatorInstance = Activator.CreateInstance(carCreatorType);
+            var carTypeField = carCreatorType.GetField("CarType", BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+            var carType = (Type?)carTypeField!.GetValue(carCreatorInstance);
+
+            fieldsFlowLayoutPanel.Controls.Clear();
+            AddCarFields(carType);
+        }
+
+        private void AddCarFields(Type? type)
+        {
+            if (type!.BaseType != typeof(Car.Car).BaseType)
+            {
+                AddCarFields(type.BaseType);
+            }
+
+            foreach (var field in type.GetProperties())
+            {
+                if (type.BaseType != null && type.BaseType.GetProperty(field.Name, BindingFlags.Public | BindingFlags.Instance) != null)
+                {
+                    continue; 
+                }
+
+                var textBox = new TextBox();
+                textBox.Font = new Font("Segoe UI", 13.8F);
+                textBox.PlaceholderText = field.Name;
+                textBox.Size = new Size(fieldsFlowLayoutPanel.Width - 30, 40);
+                textBox.TextChanged += textBox_TextChanged;
+
+                fieldsFlowLayoutPanel.Controls.Add(textBox);
+            }
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            addButton.Enabled = CanAdd();
+        }
+
+        private bool CanAdd()
+        {
+            foreach (var control in fieldsFlowLayoutPanel.Controls)
+            {
+                if (((TextBox)control).Text.Length == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            var fields = new object[fieldsFlowLayoutPanel.Controls.Count];
+            for (int i = 0; i < fields.Length; i++)
+            {
+                fields[i] = ((TextBox)fieldsFlowLayoutPanel.Controls[i]).Text;
+            }
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var carCreatorType = assembly.GetType($"Autopark.Car.{carTypeComboBox.SelectedItem}");
+
+            Program.Cars!.Add(((CarCreator)Activator.CreateInstance(carCreatorType))!.Create(fields));
+
+            this.Close();
+        }
+    }
+}
